19c6a8ad93b1c3cca3fc32f4076d67175ff7efc9
lcarrafabr <lcarrafa.br@gmail.com>
2020-07-09 17:58:33 -0300

    Tela de cadastro de lançamentos editando lançamentos

--- src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -85,17 +85,36 @@ export class LancamentoCadastroComponent implements OnInit {
   }
 
   salvar(form: FormControl) {
+
+    if (this.editando) {
+      this.atualizarlancamento(form);
+    } else {
+      this.adicionarLancamento(form);
+    }
+
+  }
+
+  adicionarLancamento(form: FormControl) {
     
     this.lancamentoService.adicionar(this.lancamento)
     .then(() => {
       this.toasty.success('Lançamento cadastrado com sucesso!')
 
       form.reset();
-
       this.lancamento = new Lancamento();
     })
     .catch(erro => this.errorhandler.handle(erro));
+  }
 
+  atualizarlancamento(form: FormControl) {
+
+    this.lancamentoService.atualizar(this.lancamento)
+    .then(lancamento => {
+      this.lancamento = lancamento;
+
+      this.toasty.success('Lancamento atualizado com sucesso!');
+    })
+    .catch(erro => this.errorhandler.handle(erro));
   }
 
 }
