142c9fd3b2622e7722e88e3a93ea2e6b07e472b8
lcarrafabr <lcarrafa.br@gmail.com>
2020-10-18 13:30:46 -0300

    Deploy

--- src/environments/server.js
+++ src/environments/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/algamoney-ui'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(__dirname + '/dist/index.html');
+  res.sendFile(__dirname + '/dist/index.html');
 });
 
-app.listen(process.env.PORT || 4200, '0.0.0.0', function() {
-    console.log('A porta sendo usada é: ' + process.env.PORT || 4200);
-});
+app.listen(process.env.PORT || 4200);
