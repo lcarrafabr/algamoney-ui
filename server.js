7af51ad5e97374e7a2249f8b5a4509c075652e04
lcarrafabr <lcarrafa.br@gmail.com>
2020-10-19 09:41:21 -0300

    Deploy

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(__dirname + '/dist/index.html');
+  res.sendFile(__dirname + '/dist/index.html');
 });
 
-app.listen(4200);
+app.listen(process.env.PORT || 4200);
